dcafb61d70cf6507e68c0da6b1218af8ff2287d3
lioga <behlulsena09@hotmail.com>
2023-10-20 14:29:27 +0300

    domain fix

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'https://lioga.github.io/behlul-blog/',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'behlulakbudak.com',
   title: 'Behlul Akbudak',
   subtitle: 'An Electronics Engineer Blog',
   lang: 'en-US',
